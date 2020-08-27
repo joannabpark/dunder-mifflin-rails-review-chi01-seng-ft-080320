e4e8cf317b055276aecf482901d7a9fcff4e2f71
joannabpark <joannabpark1@gmail.com>
2020-08-27 16:09:47 -0500

    Working on bonus

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,18 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all 
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def new_index
+        # @sorted_dogs = Dog.joins(:employees).group("employees.dog_id").order("count(employee.dog_id)desc")
+        render new_index
+    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :dogs
+  get "/dogs_sorted", to: "dogs#new_index", as: "sorted_dog"
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
